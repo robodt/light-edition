89a65ab25ccd91ca4989a67537965c3debe8f03f
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2013-11-22 19:05:43 +0100

    Adapter Framework Slim API changed bootstrap command to $app->run()

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ $site = __dir__ . DIRECTORY_SEPARATOR . 'site';
 require 'vendor/autoload.php';
 
 // Bootstrap Robodt site
-$cms = new Robodt\Adapter\Framework\Slim\AdapterFrameworkSlim($site);
+$app->run();
